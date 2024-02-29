d71f4a9b65d283958a50f96df2427974e64ab7bf
SamolasGit <132099335+SamolasGit@users.noreply.github.com>
2024-02-29 12:55:35 -0300

    Add files via upload

---
+++ Freddy/Player/Scripts/Player.gd
@@ -0,0 +1,32 @@
+extends CharacterBody2D
+
+var JumpCount = 3
+const SPEED = 200
+const JUMP_VELOCITY = -500.0
+var dir = 1
+var gravity = 980
+@onready var Animator = $AnimatedSprite2D
+
+func _physics_process(delta):
+	if is_on_wall(): 
+		dir *= -1
+		Animator.flip_h = !Animator.flip_h
+	velocity.x = dir * SPEED
+	if !is_on_floor():
+		velocity.y += gravity * delta
+		Animator.play("Jump")
+	else:
+		Animator.play("Walk")
+	if is_on_floor() and Input.is_action_just_pressed("Jump") and JumpCount > 0:
+		velocity.y += JUMP_VELOCITY 
+		JumpCount -= 1
+	move_and_slide()
+
+
+
+func _on_area_2d_body_entered(body):
+	if body.is_in_group("Killable"):
+		body.get_parent().die()
+
+	elif body.is_in_group("Spike"):
+		queue_free()
---
+++ Freddy/Player/Scripts/PneuSpike.gd
@@ -0,0 +1,24 @@
+extends CharacterBody2D
+
+@onready var Animator = $AnimatedSprite2D
+const SPEED = 100
+const JUMP_VELOCITY = -400.0
+var gravity = 980
+var dir = 1
+
+func _physics_process(delta):
+	if is_on_wall():
+		dir *= -1
+		
+		
+	if dir > 0:
+		Animator.play("SpikeTire")
+	else:
+		Animator.play_backwards("SpikeTire")
+		
+	velocity.x = dir * SPEED
+	if !is_on_floor():
+		velocity.y += gravity * delta
+	move_and_slide()
+	
+
